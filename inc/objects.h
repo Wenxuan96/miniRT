7dc3f8e7c829b9a34bcb22953bf1e059cbac34b2
lydia <lydiavdvegt@gmail.com>
2025-10-08 15:03:17 +0200

    added headers, created data structure for sphere

--- inc/objects.h
+++ inc/objects.h
@@ -1,18 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   objects.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lyvan-de <lyvan-de@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/01 16:44:46 by lyvan-de          #+#    #+#             */
-/*   Updated: 2025/10/08 13:40:05 by lyvan-de         ###   ########.fr       */
+/*   Created: 2025/10/08 14:40:25 by lyvan-de          #+#    #+#             */
+/*   Updated: 2025/10/08 15:01:57 by lyvan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/miniRT.h"
+#ifndef OBJECTS_H
+# define OBJECTS_H
 
-int32_t	main(void)
+typedef struct s_vec3
 {
-	return (0);
-}
+	double	x;
+	double	y;
+	double	z;
+}	t_vec3;
+
+typedef struct s_rgb
+{
+	int	r;
+	int	g;
+	int	b;
+}	t_rgb;
+
+typedef struct s_sphere
+{
+	t_vec3	position;
+	double	diameter;
+	t_rgb	color;
+}	t_sphere;
+
+typedef struct s_camera
+{
+	t_vec3	position;
+	t_vec3	orientation;
+	int		fov;
+}	t_camera;
+
+#endif
