56d9a1a78ade9b5ec67dde0057b9d36c8025184f
Lydia Van Der Vegt <lyvan-de@f1r6s8.codam.nl>
2025-10-13 11:22:42 +0200

    fixed norminette

--- viewport.c
+++ viewport.c
@@ -6,7 +6,7 @@
 /*   By: lyvan-de <lyvan-de@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 14:42:08 by lyvan-de          #+#    #+#             */
-/*   Updated: 2025/10/11 19:02:26 by lyvan-de         ###   ########.fr       */
+/*   Updated: 2025/10/13 11:21:56 by lyvan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 #include "inc/objects.h"
 #include "inc/math.h"
 
-double  deg_to_rad(double degrees)
+double	deg_to_rad(double degrees)
 {
-    return(degrees * PI / 180);
+	return (degrees * PI / 180);
 }
 
 t_vec3	upper_left(t_viewport *viewport, t_camera *cam)
@@ -29,23 +29,22 @@ t_vec3	upper_left(t_viewport *viewport, t_camera *cam)
 	return (upper_left);
 }
 
-t_viewport  *set_viewport(t_camera *camera)
+t_viewport	*set_viewport(t_camera *camera)
 {
-    static t_viewport   viewport;
-    double              d;
-    double              fov_rad;
-    double              aspect_ratio;
+	static t_viewport	viewport;
+	double				d;
+	double				fov_rad;
+	double				aspect_ratio;
 
-    d = 1.0;
-    fov_rad = deg_to_rad(camera->fov);
-    viewport.plane_width = 2 * d * tan(fov_rad/2);
-    aspect_ratio = (double)HEIGHT / (double)WIDTH;
-    viewport.plane_height = viewport.plane_width * aspect_ratio;
-	viewport.viewport_u = vec3(viewport.plane_width, 0 ,0);
+	d = 1.0;
+	fov_rad = deg_to_rad(camera->fov);
+	viewport.plane_width = 2 * d * tan(fov_rad / 2);
+	aspect_ratio = (double)HEIGHT / (double)WIDTH;
+	viewport.plane_height = viewport.plane_width * aspect_ratio;
+	viewport.viewport_u = vec3(viewport.plane_width, 0, 0);
 	viewport.viewport_v = vec3(0, -viewport.plane_height, 0);
 	viewport.pixel_delta_u = vec3_div(viewport.viewport_u, WIDTH);
 	viewport.pixel_delta_v = vec3_div(viewport.viewport_v, HEIGHT);
 	viewport.upper_left = upper_left(&viewport, camera);
-    return (&viewport);
+	return (&viewport);
 }
-
