34ec60b76d63d121663fed8a23a66dc91e4eed12
Lydia Van Der Vegt <lyvan-de@f1r1s16.codam.nl>
2025-10-14 16:48:30 +0200

    changed hit_sphere to calculate the distance of the camera to the sphere instead of a bool hit or not

--- ray.c
+++ ray.c
@@ -6,13 +6,13 @@
 /*   By: lyvan-de <lyvan-de@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 15:38:19 by lyvan-de          #+#    #+#             */
-/*   Updated: 2025/10/14 16:38:32 by lyvan-de         ###   ########.fr       */
+/*   Updated: 2025/10/14 16:47:00 by lyvan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/miniRT.h"
 
-bool	hit_sphere(t_vec3 ray, t_camera *cam)
+double	hit_sphere(t_vec3 ray, t_camera *cam)
 {
 	t_sphere	*sph;
 	t_vec3		origin_center;
@@ -26,18 +26,23 @@ bool	hit_sphere(t_vec3 ray, t_camera *cam)
 	radius = sph->diameter / 2;
 	origin_center = vec3_sub(cam->position, sph->position);
 	a = vec3_dot(ray, ray);
-	b = -2.0 * vec3_dot(ray, origin_center);
+	b = 2.0 * vec3_dot(ray, origin_center);
 	c = vec3_dot(origin_center, origin_center) - (radius * radius);
 	discriminant = b * b - 4 * a * c;
-	return (discriminant >= 0);
+	if (discriminant < 0)
+		return (-1.0);
+	else
+		return ((-b - sqrt(discriminant)) / (2.0 * a));
 }
 
 t_rgb	ray_rgb(t_vec3 ray, t_camera *cam)
 {
 	double	t;
 	t_rgb	rgb;
+	t_vec3	hit_point;
 
-	if (!hit_sphere(ray, cam))
+	t = hit_sphere(ray, cam);
+	if (t <= 0.0)
 	{
 		t = 0.5 * (ray.y + 1);
 		rgb.r = (int)((1.0 - t) * 255 + t * 127);
