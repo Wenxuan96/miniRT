d113d9dbdcf7333c6f314f09486b5eea906783e7
Lydia van der Vegt <lydiavdvegt@gmail.com>
2026-01-24 16:40:23 +0100

    norminette sphere.c

--- src/objects/lighting.c
+++ src/objects/lighting.c
@@ -6,7 +6,7 @@
 /*   By: lyvan-de <lyvan-de@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 13:34:26 by lyvan-de          #+#    #+#             */
-/*   Updated: 2026/01/24 15:13:37 by lyvan-de         ###   ########.fr       */
+/*   Updated: 2026/01/24 15:25:59 by lyvan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,11 @@ double	light_intensity(t_tuple hit_point, t_tuple norm_hp,
 	double		light_intensity;
 	t_light		li;
 	t_ray		shadow_ray;
-	double		distance_to_light;
 
 	li = world->light;
 	light_direction = tuple_sub(li.origin, hit_point);
 	light_direction = tuple_norm(light_direction);
-	light_intensity = fmax(0.0, tuple_dot(norm_hp, light_direction))
-		* li.ratio;
-	distance_to_light = tuple_lenght(light_direction);
+	light_intensity = fmax(0.0, tuple_dot(norm_hp, light_direction)) * li.ratio;
 	shadow_ray.origin = tuple_add(hit_point, tuple_mult(norm_hp, EPSILON));
 	shadow_ray.direction = light_direction;
 	if (in_shadow(shadow_ray, obj, world))
--- src/objects/sphere.c
+++ src/objects/sphere.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ray_sphere.c                                       :+:      :+:    :+:   */
+/*   sphere.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lyvan-de <lyvan-de@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 15:40:51 by wxi               #+#    #+#             */
-/*   Updated: 2026/01/20 15:18:25 by lyvan-de         ###   ########.fr       */
+/*   Updated: 2026/01/24 16:38:57 by lyvan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/miniRT.h"
 
-void	init_sphere_transform(t_sphere *sphere)
+void	init_sphere_transform(t_sphere *sph)
 {
 	double		radius;
 	t_matrix4	scale_mat;
 	t_matrix4	trans_mat;
-	
-	radius = sphere->diameter / 2;
+
+	radius = sph->diameter / 2;
 	scale_mat = mat_scaling(radius, radius, radius);
-	trans_mat = translation(sphere->position.x, sphere->position.y, sphere->position.z);
-	sphere->base.transform = multi_mat(trans_mat, scale_mat);
-	sphere->base.inv_transform = invert_m4(sphere->base.transform);
+	trans_mat = translation(sph->position.x, sph->position.y, sph->position.z);
+	sph->base.transform = multi_mat(trans_mat, scale_mat);
+	sph->base.inv_transform = invert_m4(sph->base.transform);
 }
 
 double	intersect_unit_sphere(t_ray	*ray)
@@ -31,15 +31,12 @@ double	intersect_unit_sphere(t_ray	*ray)
 	double	b;
 	double	c;
 	double	discriminant;
-	t_tuple		origin_center;
-	t_tuple		zero_pos;
-	
-	zero_pos = new_tuple(0, 0, 0, 1);
-	origin_center = tuple_sub(ray->origin, zero_pos);
+	t_tuple	origin_center;
+
+	origin_center = tuple_sub(ray->origin, new_tuple(0, 0, 0, 1));
 	a = tuple_dot(ray->direction, ray->direction);
 	b = 2.0 * tuple_dot(ray->direction, origin_center);
 	c = tuple_dot(origin_center, origin_center) - 1.0;
-	
 	discriminant = b * b - 4 * a * c;
 	if (discriminant < 0)
 		return (-1);
@@ -47,5 +44,3 @@ double	intersect_unit_sphere(t_ray	*ray)
 	ray->hit_points[1] = (-b + sqrt(discriminant)) / (2.0 * a);
 	return (select_t(ray->hit_points[0], ray->hit_points[1]));
 }
-
-
