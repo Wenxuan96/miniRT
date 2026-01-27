77677fb08e34b56ac222b6a138eeda3098730d53
Lydia van der Vegt <lydiavdvegt@gmail.com>
2026-01-27 12:58:40 +0100

    norminette vector directory

--- src/vectors/vec_utils2.c
+++ src/vectors/vec_utils2.c
@@ -6,7 +6,7 @@
 /*   By: lyvan-de <lyvan-de@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 15:54:21 by lyvan-de          #+#    #+#             */
-/*   Updated: 2026/01/22 19:18:36 by lyvan-de         ###   ########.fr       */
+/*   Updated: 2026/01/27 12:58:16 by lyvan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,13 @@ t_tuple	tuple_cross(t_tuple a, t_tuple b)
 	t_tuple	c;
 
 	c.x = a.y * b.z - a.z * b.y;
-    c.y = a.z * b.x - a.x * b.z;
-    c.z = a.x * b.y - a.y * b.x;
+	c.y = a.z * b.x - a.x * b.z;
+	c.z = a.x * b.y - a.y * b.x;
 	c.w = 0;
-
 	return (c);
 }
 
 double	tuple_lenght(t_tuple a)
 {
-	return(sqrt(tuple_dot(a, a)));
+	return (sqrt(tuple_dot(a, a)));
 }
